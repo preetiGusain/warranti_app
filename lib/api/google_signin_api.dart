cbb034bad370ff76d7c82dd515d5bd096eef0911
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-12-27 14:52:33 +0530

    Adding packages and dependencies

---
+++ lib/api/google_signin_api.dart
@@ -0,0 +1,6 @@
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleSigninApi {
+  static final _googleSignIn = GoogleSignIn();
+  static Future login() => _googleSignIn.signIn();
+}

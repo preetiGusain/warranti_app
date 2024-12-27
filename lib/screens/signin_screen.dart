7b6816733348711921283fb2d18e87a022cf1d33
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-12-27 12:20:43 +0530

    Using the store token service in signin

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:icons_plus/icons_plus.dart';
+import 'package:warranti_app/api/google_signIn_api.dart';
 import 'package:warranti_app/screens/signup_screen.dart';
+import 'package:warranti_app/service/store_token.dart';
 import 'package:warranti_app/theme/theme.dart';
 import 'package:warranti_app/widgets/custom_scaffold.dart';
+import 'package:http/http.dart' as http;
 
 class SigninScreen extends StatefulWidget {
   const SigninScreen({super.key});
@@ -18,213 +21,262 @@ class _SigninScreenState extends State<SigninScreen> {
   Widget build(BuildContext context) {
     return CustomScaffold(
       child: Column(
-      children: [
-        const Expanded(
-          flex: 1,
-          child: SizedBox(
-            height: 10,
+        children: [
+          const Expanded(
+            flex: 1,
+            child: SizedBox(
+              height: 10,
+            ),
           ),
-        ),
-        Expanded(
-          flex: 7,
-          child: Container(
-            padding: const EdgeInsets.fromLTRB(25, 50, 25, 20),
-            decoration: const BoxDecoration(
+          Expanded(
+            flex: 7,
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(25, 50, 25, 20),
+              decoration: const BoxDecoration(
                 color: Color.fromRGBO(255, 255, 255, 0.95),
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(40.0),
                   topRight: Radius.circular(40.0),
-                )),
-            child: SingleChildScrollView(
-              child: Form(
-                key: _formSignInKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Welcome back',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w900,
-                        color: lightColorScheme.primary,
+                ),
+              ),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _formSignInKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w900,
+                          color: lightColorScheme.primary,
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 25.0,
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            padding: const EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              border:
-                                  Border.all(color: Colors.black26, width: 0.5),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: Row(
-                              children: [
-                                Logo(
-                                  Logos.google,
-                                  size: 18,
+                      const SizedBox(
+                        height: 25.0,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            ElevatedButton.icon(
+                              style: ElevatedButton.styleFrom(
+                                foregroundColor: Colors.black,
+                                backgroundColor: Colors.white,
+                                padding: const EdgeInsets.all(16),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
                                 ),
-                                const SizedBox(width: 8),
-                                const Text('Sign in with Google'),
-                              ],
+                                side: const BorderSide(color: Colors.black26),
+                              ),
+                              icon: Image.asset(
+                                'assets/images/google_logo.png',
+                                height: 18,
+                              ),
+                              label: const Text(
+                                'Sign in with Google',
+                                style: TextStyle(fontSize: 16),
+                              ),
+                              onPressed: signIn,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 40.0,
+                      ),
+                      Text(
+                        'Or use your email and password',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w600,
+                          color: lightColorScheme.primary,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25.0,
+                      ),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter Email';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          label: const Text('Email'),
+                          hintText: 'Enter Email',
+                          hintStyle: const TextStyle(
+                            color: Colors.black26,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.black12,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black12,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25.0,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        obscuringCharacter: '*',
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter Password';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          label: const Text('Password'),
+                          hintText: 'Enter Password',
+                          hintStyle: const TextStyle(
+                            color: Colors.black26,
+                          ),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.black12,
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          enabledBorder: const OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.black12,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 40.0,
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: const Text("Sign In"),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 25.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              thickness: 0.7,
+                              color: Colors.grey.withOpacity(0.5),
+                            ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: 0,
+                              horizontal: 10,
+                            ),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              thickness: 0.7,
+                              color: Colors.grey.withOpacity(0.5),
                             ),
                           ),
                         ],
                       ),
-                    ),
-                    const SizedBox(
-                      height: 40.0,
-                    ),
-                    Text(
-                      'Or use your email and password',
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w600,
-                        color: lightColorScheme.primary,
+                      const SizedBox(
+                        height: 25.0,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 25.0,
-                    ),
-                    TextFormField(
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter Email';
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                        label: const Text('Email'),
-                        hintText: 'Enter Email',
-                        hintStyle: const TextStyle(
-                          color: Colors.black26,
-                        ),
-                        border: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: Colors.black12,
-                          ),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        enabledBorder: const OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.black12,
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 25.0,
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      obscuringCharacter: '*',
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Please enter Password';
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                        label: const Text('Password'),
-                        hintText: 'Enter Password',
-                        hintStyle: const TextStyle(
-                          color: Colors.black26,
-                        ),
-                        border: OutlineInputBorder(
-                          borderSide: const BorderSide(
-                            color: Colors.black12,
-                          ),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        enabledBorder: const OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.black12,
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 40.0,
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        child: const Text("Sign In"),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 25.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: Divider(
-                            thickness: 0.7,
-                            color: Colors.grey.withOpacity(0.5),
-                          ),
-                        ),
-                        const Padding(
-                            padding: EdgeInsets.symmetric(
-                          vertical: 0,
-                          horizontal: 10,
-                        )),
-                        Expanded(
-                          child: Divider(
-                            thickness: 0.7,
-                            color: Colors.grey.withOpacity(0.5),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 25.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          'Don\'t have an account',
-                          style: TextStyle(
-                            color: Colors.black45,
-                          ),
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (e) => const SignupScreen(),
-                              ),
-                            );
-                          },
-                          child: Text(
-                            'Sign up',
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Don\'t have an account',
                             style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: lightColorScheme.primary,
+                              color: Colors.black45,
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 25.0,
-                    ),
-                  ],
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (e) => const SignupScreen(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              'Sign up',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: lightColorScheme.primary,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 25.0,
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
-        ),
-      ],
-    ));
+        ],
+      ),
+    );
+  }
+
+  Future signIn() async {
+    final user = await GoogleSigninApi.login();
+    final status = await fetchTokenFromBackend(user);
+    if (status) {
+      Navigator.of(context).pushNamed('/home');
+    }
+  }
+
+  Future<bool> fetchTokenFromBackend(user) async {
+    try {
+      final response = await http.post(
+        Uri.parse('https://warranti-backend.onrender.com/oauth/google/app'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          "id": user.id,
+          "displayName": user.displayName,
+          "email": user.email,
+          "photoUrl": user.photoUrl
+        }),
+      );
+      print("response $response");
+
+      if (response.statusCode == 200) {
+        final body = response.body;
+        print("body $body");
+
+        final json = jsonDecode(body);
+        print("json $json");
+
+        if (json.containsKey('token') && json['token'] != null) {
+          await storeToken(json['token']);
+          return true;
+        }
+      } else {
+        print('Failed to fetch token: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error during request: $e');
+    }
+    return false;
   }
 }

b2cf655cfcc53bf7e746d0af1c425c7fd443180e
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-12-31 13:50:58 +0530

    Sending images in the form successfully

--- lib/screens/create_screen.dart
+++ lib/screens/create_screen.dart
@@ -17,7 +17,9 @@ class _CreateScreenState extends State<CreateScreen> {
   DateTime? selectedDate;
   String warrantyDuration = '';
   String selectedUnit = 'Month';
-  File? selectedImage;
+  File? productPhoto;
+  File? warrantyCardPhoto;
+  File? receiptPhoto;
 
   Future<void> selectDate(BuildContext context) async {
     final DateTime initialDate = selectedDate ?? DateTime.now();
@@ -45,6 +47,7 @@ class _CreateScreenState extends State<CreateScreen> {
       print('No token found');
       return;
     }
+    print('Using token: $token');
 
     final url = Uri.parse('$backend_uri/warranty/create');
     final request = http.MultipartRequest('POST', url)
@@ -57,9 +60,25 @@ class _CreateScreenState extends State<CreateScreen> {
     request.fields['warrantyDuration'] = warrantyDuration;
     request.fields['warrantyDurationUnit'] = selectedUnit;
 
+    if (productPhoto != null) {
+      request.files.add(
+          await http.MultipartFile.fromPath('product', productPhoto!.path));
+    }
+    if (warrantyCardPhoto != null) {
+      request.files.add(await http.MultipartFile.fromPath(
+          'warrantyCard', warrantyCardPhoto!.path));
+    }
+    if (receiptPhoto != null) {
+      request.files.add(
+          await http.MultipartFile.fromPath('receipt', receiptPhoto!.path));
+    }
+
     try {
       final response = await request.send();
       final responseBody = await http.Response.fromStream(response);
+
+      print('Response status: ${response.statusCode}');
+      print('Response body: ${responseBody.body}');
 
       if (response.statusCode == 200) {
         print('Success: ${responseBody.body}');
@@ -73,25 +92,22 @@ class _CreateScreenState extends State<CreateScreen> {
     }
   }
 
-  Future pickImageFromGallery() async {
-    final returnedImage =
-        await ImagePicker().pickImage(source: ImageSource.gallery);
+  Future pickImage(String imageType, ImageSource source) async {
+    final returnedImage = await ImagePicker().pickImage(source: source);
     if (returnedImage == null) return;
+
     setState(() {
-      selectedImage = File(returnedImage!.path);
+      if (imageType == 'product') {
+        productPhoto = File(returnedImage.path);
+      } else if (imageType == 'warrantyCard') {
+        warrantyCardPhoto = File(returnedImage.path);
+      } else if (imageType == 'receipt') {
+        receiptPhoto = File(returnedImage.path);
+      }
     });
   }
 
-  Future pickImageFromCamera() async {
-    final returnedImage =
-        await ImagePicker().pickImage(source: ImageSource.camera);
-    if (returnedImage == null) return;
-    setState(() {
-      selectedImage = File(returnedImage!.path);
-    });
-  }
-
-  void showImageSourceOptions(BuildContext context) {
+  void showImageSourceOptions(BuildContext context, String imageType) {
     showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
@@ -104,7 +120,7 @@ class _CreateScreenState extends State<CreateScreen> {
                 title: const Text('Camera'),
                 onTap: () async {
                   Navigator.pop(context);
-                  await pickImageFromCamera();
+                  await pickImage(imageType, ImageSource.camera);
                 },
               ),
               ListTile(
@@ -112,7 +128,7 @@ class _CreateScreenState extends State<CreateScreen> {
                 title: const Text('Gallery'),
                 onTap: () async {
                   Navigator.pop(context);
-                  await pickImageFromGallery();
+                  await pickImage(imageType, ImageSource.gallery);
                 },
               ),
             ],
@@ -129,91 +145,87 @@ class _CreateScreenState extends State<CreateScreen> {
         title: const Text('Create Warranty'),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              keyboardType: TextInputType.text,
-              decoration: const InputDecoration(
-                labelText: 'Product Name',
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                  labelText: 'Product Name',
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    productName = value;
+                  });
+                },
               ),
-              onChanged: (value) {
-                setState(() {
-                  productName = value;
-                });
-              },
-            ),
-            const SizedBox(height: 20),
-            GestureDetector(
-              onTap: () => selectDate(context),
-              child: AbsorbPointer(
-                child: TextField(
-                  controller: TextEditingController(
-                    text: selectedDate == null
-                        ? ''
-                        : '${selectedDate?.toLocal()}'.split(' ')[0],
+              const SizedBox(height: 20),
+              GestureDetector(
+                onTap: () => selectDate(context),
+                child: AbsorbPointer(
+                  child: TextField(
+                    controller: TextEditingController(
+                      text: selectedDate == null
+                          ? ''
+                          : '${selectedDate?.toLocal()}'.split(' ')[0],
+                    ),
+                    decoration: const InputDecoration(
+                      labelText: 'Purchase Date',
+                      hintText: 'Tap to select a date',
+                    ),
+                    readOnly: true,
                   ),
-                  decoration: const InputDecoration(
-                    labelText: 'Purchase Date',
-                    hintText: 'Tap to select a date',
-                  ),
-                  readOnly: true,
                 ),
               ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: 'Warranty Duration',
+              const SizedBox(height: 20),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  labelText: 'Warranty Duration',
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    warrantyDuration = value;
+                  });
+                },
               ),
-              onChanged: (value) {
-                setState(() {
-                  warrantyDuration = value;
-                });
-              },
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Warranty Duration Unit',
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            DropdownButton<String>(
-              value: selectedUnit,
-              items: <String>['Month', 'Year'].map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-              onChanged: (value) {
-                setState(() {
-                  selectedUnit = value ?? 'Month';
-                });
-              },
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                if (productName.isNotEmpty &&
-                    warrantyDuration.isNotEmpty &&
-                    selectedDate != null) {
-                  createWarranty();
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Please fill all fields')),
+              const SizedBox(height: 20),
+              const Text(
+                'Warranty Duration Unit',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8),
+              DropdownButton<String>(
+                value: selectedUnit,
+                items: <String>['Month', 'Year'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
                   );
-                }
-              },
-              child: const Text('Save'),
-            ),
-            MaterialButton(
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    selectedUnit = value ?? 'Month';
+                  });
+                },
+              ),
+              const SizedBox(height: 20),
+              productPhoto != null
+                  ? Image.file(
+                      productPhoto!,
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
+                    )
+                  : const Text("No Product Image Selected"),
+              MaterialButton(
                 color: Colors.lightBlue,
                 onPressed: () {
-                  showImageSourceOptions(context);
+                  showImageSourceOptions(context, 'product');
                 },
                 child: const Text(
                   "Select Product Image",
@@ -222,12 +234,71 @@ class _CreateScreenState extends State<CreateScreen> {
                     fontWeight: FontWeight.bold,
                     fontSize: 16,
                   ),
-                )),
-            const SizedBox(height: 20),
-            selectedImage != null
-                ? Image.file(selectedImage!)
-                : const Text("Please select an Image")
-          ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              warrantyCardPhoto != null
+                  ? Image.file(
+                      warrantyCardPhoto!,
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
+                    )
+                  : const Text("No Warranty Card Image Selected"),
+              MaterialButton(
+                color: Colors.lightBlue,
+                onPressed: () {
+                  showImageSourceOptions(context, 'warrantyCard');
+                },
+                child: const Text(
+                  "Select Warranty Card Image",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              receiptPhoto != null
+                  ? Image.file(
+                      receiptPhoto!,
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.cover,
+                    )
+                  : const Text("No Receipt Image Selected"),
+              MaterialButton(
+                color: Colors.lightBlue,
+                onPressed: () {
+                  showImageSourceOptions(context, 'receipt');
+                },
+                child: const Text(
+                  "Select Receipt Image",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (productName.isNotEmpty &&
+                      warrantyDuration.isNotEmpty &&
+                      selectedDate != null) {
+                    createWarranty();
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Please fill all fields')),
+                    );
+                  }
+                },
+                child: const Text('Save'),
+              ),
+            ],
+          ),
         ),
       ),
     );
